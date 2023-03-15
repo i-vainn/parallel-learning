de8c97fa228cb075383c493b5b3f2f8438cae527
ivan7022 <1vanmoshkovv@mail.ru>
2023-03-15 00:23:56 +0000

    new entries

--- ddp_cifar100.py
+++ ddp_cifar100.py
@@ -1,5 +1,8 @@
 import os
+import gc
+import argparse
 
+import wandb
 import torch
 import torch.distributed as dist
 import torch.nn as nn
@@ -8,15 +11,23 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
 from torchvision.datasets import CIFAR100
+from tqdm.autonotebook import tqdm
+from time import perf_counter
+
+from syncbn import SyncBatchNorm
+
 
 torch.set_num_threads(1)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
+])
 
-
-def init_process(local_rank, fn, backend="nccl"):
+def init_process(local_rank, fn, backend="nccl", **kwargs):
     """Initialize the distributed environment."""
     dist.init_process_group(backend, rank=local_rank)
     size = dist.get_world_size()
-    fn(local_rank, size)
+    fn(local_rank, size, **kwargs)
 
 
 class Net(nn.Module):
@@ -31,9 +42,11 @@ class Net(nn.Module):
         self.conv2 = nn.Conv2d(32, 32, 3, 1)
         self.dropout1 = nn.Dropout(0.25)
         self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(6272, 128)
-        self.fc2 = nn.Linear(128, 100)
-        self.bn1 = nn.BatchNorm1d(128, affine=False)  # to be replaced with SyncBatchNorm
+        hidden_size = 1024
+        self.fc1 = nn.Linear(6272, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, 100)
+        # self.bn1 = nn.SyncBatchNorm(hidden_size, affine=False)
+        self.bn1 = SyncBatchNorm(hidden_size)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -54,58 +67,117 @@ class Net(nn.Module):
         return output
 
 
-def average_gradients(model):
-    size = float(dist.get_world_size())
+def average_gradients(model, accumulation_steps=1):
+    size = float(dist.get_world_size()) * accumulation_steps
     for param in model.parameters():
         dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
         param.grad.data /= size
 
 
-def run_training(rank, size):
+def run_training(rank, size, grad_accumulation_steps, device, batch_size, num_workers):
+    device += f':{rank}'
     torch.manual_seed(0)
 
-    dataset = CIFAR100(
-        "./cifar",
-        transform=transforms.Compose(
-            [
-                transforms.ToTensor(),
-                transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
-            ]
-        ),
-        download=True,
+    train_dataset = CIFAR100(
+        "./cifar", transform=transform, download=True, train=True,
+    )
+    val_dataset = CIFAR100(
+        "./cifar", transform=transform, download=True, train=False,
     )
     # where's the validation dataset?
-    loader = DataLoader(dataset, sampler=DistributedSampler(dataset, size, rank), batch_size=64)
+    train_loader = DataLoader(train_dataset, sampler=DistributedSampler(train_dataset, size, rank),
+                              batch_size=batch_size, num_workers=num_workers)
+    val_loader = DataLoader(val_dataset, sampler=DistributedSampler(val_dataset, size, rank),
+                            batch_size=batch_size, num_workers=num_workers)
 
     model = Net()
-    device = torch.device("cpu")  # replace with "cuda" afterwards
     model.to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
 
-    num_batches = len(loader)
+    batch_number = 0
 
-    for _ in range(10):
-        epoch_loss = torch.zeros((1,), device=device)
+    for epoch in range(10):
+        gc.collect()
+        torch.cuda.empty_cache()
+        torch.cuda.reset_max_memory_allocated()
+        torch.cuda.synchronize()
+        start_time = perf_counter()
+        epoch_loss = torch.zeros(1, device=device)
 
-        for data, target in loader:
+        for data, target in tqdm(train_loader, total=len(train_loader), desc=f'Train {epoch}'):
+            batch_number += 1
             data = data.to(device)
             target = target.to(device)
 
-            optimizer.zero_grad()
             output = model(data)
             loss = torch.nn.functional.cross_entropy(output, target)
             epoch_loss += loss.detach()
             loss.backward()
-            average_gradients(model)
-            optimizer.step()
+
+            if batch_number % grad_accumulation_steps == 0:
+                average_gradients(model, grad_accumulation_steps)
+                optimizer.step()
+                optimizer.zero_grad()
 
             acc = (output.argmax(dim=1) == target).float().mean()
 
-            print(f"Rank {dist.get_rank()}, loss: {epoch_loss / num_batches}, acc: {acc}")
-            epoch_loss = 0
-        # where's the validation loop?
+            # print(f"Rank {dist.get_rank()}, loss: {epoch_loss / num_batches}, acc: {acc}")
 
+        torch.cuda.synchronize()
+        elapsed_time = perf_counter() - start_time
+
+        val_loss = torch.zeros(1, device=device)
+        hits = torch.zeros(1, device=device)
+        samples = torch.zeros(1, device=device)
+        for data, target in tqdm(val_loader, total=len(val_loader), desc=f'Validation {epoch}'):
+            data = data.to(device)
+            target = target.to(device)
+
+            with torch.no_grad():
+                output = model(data)
+                loss = torch.nn.functional.cross_entropy(output, target, reduction='sum')
+                val_loss += loss
+                hits += (output.argmax(dim=1) == target).float().sum()
+                samples += output.size(0)
+
+        msg_tensor = torch.cat([val_loss, hits, samples], dim=0)
+        dist.all_reduce(msg_tensor,  dist.ReduceOp.SUM)
+        global_val_loss, global_hits, global_samples = torch.split(msg_tensor, 1)
+
+        if local_rank == 0:
+            memory = torch.cuda.memory_allocated() / 1024 / 1024
+            wandb.log(dict(
+                    train_loss=epoch_loss/len(train_loader), 
+                    global_val_loss=global_val_loss / global_samples,
+                    global_accuracy=global_hits / global_samples,
+                    elapsed_time=elapsed_time, 
+                    memory=memory,
+            ))
+
+def parse_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--grad-accumulation-steps', default=1, type=int)
+    argparser.add_argument('--batch-size', type=int, default=64)
+    argparser.add_argument('--num-workers', type=int, default=0)
+    argparser.add_argument('--device', type=str, default='cpu')
+    argparser.add_argument('--name', type=str, default='DEBUG')
+    return argparser.parse_args()
 
 if __name__ == "__main__":
+    args = parse_args()
     local_rank = int(os.environ["LOCAL_RANK"])
-    init_process(local_rank, fn=run_training, backend="gloo")  # replace with "nccl" when testing on GPUs
+
+    if local_rank == 0:
+        run = wandb.init(
+            entity='i_vainn',
+            project="parallel-training",
+            name=args.name,
+            config=args.__dict__
+        )
+    
+    init_process(
+        local_rank=local_rank, fn=run_training,
+        backend="gloo" if args.device == 'cpu' else 'nccl',
+        grad_accumulation_steps=args.grad_accumulation_steps, 
+        device=args.device, batch_size=args.batch_size, num_workers=args.num_workers
+    )
--- syncbn.py
+++ syncbn.py
@@ -20,30 +20,48 @@ class sync_batch_norm(Function):
     def forward(ctx, input, running_mean, running_std, eps: float, momentum: float):
         # Compute statistics, sync statistics, apply them to the input
         # Also, store relevant quantities to be used on the backward pass with `ctx.save_for_backward`
-        mean = input.mean(1)
-        mean_squares = torch.square(input).mean(1)
-        communication_tensor = torch.cat([mean, mean_squares])
-        dist.all_reduce(communication_tensor)
-        communication_tensor /= dist.get_world_size()
-        cur_mean = communication_tensor[:mean.size(0)]
-        cur_var = communication_tensor[mean.size(0):] - cur_mean ** 2
-        norm_input = (input - cur_mean) / torch.sqrt(cur_var + eps)
-        ctx.save_for_backward(cur_mean, cur_var, norm_input, running_std)
+        
+        sum = input.sum(0)
+        sum_squares = torch.square(input).sum(0)
+        batch_msg = torch.tensor([input.shape[0]], device=input.device)
 
-        return norm_input * running_std + running_mean
+        msg_tensor = torch.cat([
+            sum, sum_squares, batch_msg
+        ])
+        dist.all_reduce(msg_tensor)
+        global_sum, global_sum_squares, global_batch_size = torch.split(msg_tensor, input.shape[1])
+
+        global_mean = global_sum / global_batch_size.item()
+        global_var = (global_sum_squares - (global_sum ** 2) / global_batch_size) / global_batch_size
+        global_std = torch.sqrt(global_var + eps)
+
+        running_mean = running_mean * (1 - momentum) + global_mean * momentum
+        running_std = running_std * (1 - momentum) + global_std * momentum
+        
+        norm_input = (input - running_mean) / running_std
+        ctx.save_for_backward(norm_input, running_std, global_batch_size)
+
+        return norm_input
 
     @staticmethod
     def backward(ctx, grad_output):
-        cur_mean, cur_var, norm_input, running_std = ctx.saved_tensors
-        grad_running_std = norm_input * grad_output
-        grad_running_mean = norm_input
-        grad_output = grad_output * running_std
-        # https://pytorch.org/docs/stable/generated/torch.nn.SyncBatchNorm.html
-        # https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/batchnorm.py
-        # https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/_functions.py
-        # https://kevinzakka.github.io/2016/09/14/batch_normalization/
-        # https://pytorch.org/docs/stable/generated/torch.autograd.function.FunctionCtx.save_for_backward.html
-        
+        norm_input, global_std, global_batch_size = ctx.saved_tensors
+
+        sum_gout = grad_output.sum(0)
+        sum_prod_gout = (grad_output * norm_input).sum(0)
+
+        msg_tensor = torch.cat([
+            sum_gout, sum_prod_gout
+        ])
+        dist.all_reduce(msg_tensor)
+        sum_gout, sum_prod_gout = torch.split(msg_tensor, norm_input.size(1))
+
+        grad_output = (
+            global_batch_size * grad_output - 
+            sum_gout - norm_input * sum_prod_gout
+        ) / global_batch_size / global_std
+
+        return grad_output, None, None, None, None
 
 
 class SyncBatchNorm(_BatchNorm):
@@ -52,7 +70,7 @@ class SyncBatchNorm(_BatchNorm):
     across all processes. You can assume that there are no affine operations in this layer.
     """
 
-    def __init__(self, num_features: int, eps: float = 1e-5, momentum: float = 0.1):
+    def __init__(self, num_features: int, eps: float = 1e-5, momentum: float = 1.):
         super().__init__(
             num_features,
             eps,
@@ -65,7 +83,11 @@ class SyncBatchNorm(_BatchNorm):
         # your code here
         self.running_mean = torch.zeros((num_features,))
         self.running_std = torch.ones((num_features,))
+        self.bn_func = sync_batch_norm.apply
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        # You will probably need to use `sync_batch_norm` from above
-        pass
+        if not self.training and self.track_running_stats:
+            std = torch.sqrt(self.running_var + self.eps)
+            return (input - self.running_mean) / std
+        else:
+            return self.bn_func(input, self.running_mean, self.running_var, self.eps, self.momentum)
